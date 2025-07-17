f2bb29e261a161fc08173681c49a5e8bfe4e767c
galalkoro98 <galal.koro@gmail.com>
2025-07-17 12:17:54 +0200

    added new file

---
+++ RedHat/basic_command.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Basic Command Script
+
+echo "Hello, World!"
+
+# linux commands
+ls -l
+pwd
+whoami
+# Display disk usage
+df -h
+# Display memory usage
+free -h
+# Display running processes
+ps aux
+# Display network configuration
+ifconfig
